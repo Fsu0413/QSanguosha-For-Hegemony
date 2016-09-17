1d9b4a960e8d7eef9ae5ed30ed22bbda27be030c
Fs <Fsu0413@users.noreply.github.com>
2016-09-17 14:03:08 +0800

    Revert "看起来to不会是nullptr"
    to 如果是nullptr需要做guard啊
    
    This reverts commit ef61a14e224ffa33ae7809d212b8d16bdbf0cb4d.

--- gamelogiclib/src/structs.cpp
+++ gamelogiclib/src/structs.cpp
@@ -237,6 +237,11 @@ CardEffectStruct::CardEffectStruct()
 
 QJsonValue CardEffectStruct::toJson() const
 {
+    QJsonValue v;
+
+    if (to == nullptr)
+        return v;
+
     QJsonObject ob;
 
     ob.insert(QStringLiteral("structType"), QStringLiteral("CardEffectStruct"));
